97cfede4b589a45eb7aebc3fd69231146efc2a6c
Gustavo Cassel <guscassel@gmail.com>
2021-06-11 11:09:25 -0300

    Improved function to send e-mail

--- functions/mail/src/main/java/com/openfaas/function/Handler.java
+++ functions/mail/src/main/java/com/openfaas/function/Handler.java
@@ -1,30 +1,37 @@
 package com.openfaas.function;
 
+import java.util.Optional;
+
 import javax.mail.MessagingException;
 import com.openfaas.model.IHandler;
 import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
-public class Handler implements IHandler {
+public class Handler extends com.openfaas.model.AbstractHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-        var payload = req.getBody();
-        var mailOpt = new MailFactory().createFromJsonText(payload);
-        if (mailOpt.isPresent()) {
-            var mail = mailOpt.get();
-            try {                
-                mail.send();
-                res.setStatusCode(200);
-                res.setBody("Sent OK.");
-            } catch (MessagingException e) {
+        try {
+            var payload = req.getBody();
+            var mailOpt = new MailFactory().createFromJsonText(payload);
+            // var mailOpt = Optional.empty();
+            if (mailOpt.isPresent()) {
+                var mail = mailOpt.get();
+                try {                
+                    mail.send();
+                    res.setStatusCode(200);
+                    res.setBody("Sent OK.");
+                } catch (MessagingException e) {
+                    res.setStatusCode(400);
+                    res.setBody(e.toString());
+                }
+            } else {
                 res.setStatusCode(400);
-                res.setBody(e.toString());
+                res.setBody("Not all mandatory information was supplied.");
             }
-        } else {
-            res.setStatusCode(400);
-            res.setBody("Not all mandatory information was supplied.");
+        } catch (Throwable e) {
+            res.setBody(e.toString());
         }
 	    return res;
     }
--- functions/mail/src/main/java/com/openfaas/function/MailFactory.java
+++ functions/mail/src/main/java/com/openfaas/function/MailFactory.java
@@ -1,7 +1,6 @@
 package com.openfaas.function;
 
 import java.util.Optional;
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
